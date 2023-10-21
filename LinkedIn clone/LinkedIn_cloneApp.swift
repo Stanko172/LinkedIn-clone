11bd641e0223bc32a459fa07ef8cb6588aed6223
Stanko172 <stanko.bebek00@gmail.com>
2023-10-21 10:49:07 +0200

    Initial Commit

---
+++ LinkedIn clone/LinkedIn_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedIn_cloneApp.swift
+//  LinkedIn clone
+//
+//  Created by Stanko Bebek on 21. 10. 2023..
+//
+
+import SwiftUI
+
+@main
+struct LinkedIn_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
